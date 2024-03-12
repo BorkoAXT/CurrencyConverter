aae5e48b7350c77dfd33caf72afa649c1e48b389
BorkoAXT <borkoaxtyt@gmail.com>
2024-03-12 10:48:51 +0200

    Added Project Files

---
+++ WindowsFormsApp1/ConversionForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownAmount = new System.Windows.Forms.NumericUpDown();
+            this.currency = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownAmount
+            // 
+            this.numericUpDownAmount.DecimalPlaces = 2;
+            this.numericUpDownAmount.Location = new System.Drawing.Point(92, 56);
+            this.numericUpDownAmount.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownAmount.Name = "numericUpDownAmount";
+            this.numericUpDownAmount.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownAmount.TabIndex = 0;
+            this.numericUpDownAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // currency
+            // 
+            this.currency.AutoSize = true;
+            this.currency.Location = new System.Drawing.Point(296, 62);
+            this.currency.Name = "currency";
+            this.currency.Size = new System.Drawing.Size(82, 16);
+            this.currency.TabIndex = 1;
+            this.currency.Text = "BGN to EUR";
+            this.currency.Click += new System.EventHandler(this.ConvertCurrency);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BackColor = System.Drawing.Color.PaleGreen;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(60, 96);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(402, 23);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultButton
+            // 
+            this.resultButton.Location = new System.Drawing.Point(218, 56);
+            this.resultButton.Name = "resultButton";
+            this.resultButton.Size = new System.Drawing.Size(75, 23);
+            this.resultButton.TabIndex = 3;
+            this.resultButton.Text = "Convert";
+            this.resultButton.UseVisualStyleBackColor = true;
+            this.resultButton.Click += new System.EventHandler(this.ConvertCurrency);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.currency);
+            this.Controls.Add(this.numericUpDownAmount);
+            this.Name = "Form1";
+            this.Text = "CurrencyConverter";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownAmount;
+        private System.Windows.Forms.Label currency;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button resultButton;
+    }
+}
+
---
+++ WindowsFormsApp1/ConversionForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertCurrency(object sender, EventArgs e)
+        {
+            const decimal ConversionCoefficent = 1.95583m;
+
+            decimal amountBGN = this.numericUpDownAmount.Value;
+            decimal amountEUR = amountBGN / ConversionCoefficent;
+
+            this.resultLabel.Text = $"{amountBGN} BGN = {amountEUR:F2} EUR";
+        }
+    }
+}
